17599e3db64cb8dd005f15a632d4122604cd8938
xiaovhua <nkvhua@outlook.com>
2018-11-14 14:57:36 +0800

    Update Game.py

--- bird/Game.py
+++ bird/Game.py
@@ -1,35 +1,36 @@
+# Used to describe the game into [state, action, rewards, StateTransitionMatrix, penalty] 
 import numpy as np
 class game():
     def __init__(self):
-        # A(s),行动种类
+        # types of action
         self.action = ['e', 's', 'w', 'n']
-        # 当前选择的的行动
+        # the index of the action which is taken in this step
         self.a = 0
-        # 初始化终止状态:终点,砖块
+        # the endstate, including the brick and the goal
         self.endstate_init()
-        # 状态,初始为[0, 0]
+        # current state
         self.state = [0, 0]
-        # 初始化回报
+        # rewards
         self.rewards = np.zeros([17, 20, 4])
-        # 初始化值函数
+        # State Value Function
         self.V = np.zeros([17, 20])
-        # 初始化行为值函数
+        # Action-State Valve Function
         self.Q = np.zeros([17, 20, 4])
-        # 决策时用到的策略
+        # policy to make decision
         self.policy = np.zeros([17, 20])
-        # 折扣函数
+        # penalty
         self.gama = 0.98
-        # 迭代次数
+        # iteration
         self.iteration = 0
-        # (s, a)对
+        # the (state, action) of one step
         self.s_a = []
-        # epsilon-greedy 策略的值
+        # the value of epsilon in epsilon-greedy
         self.epsilon = 0.25
-        # P状态转移矩阵
+        # the State Transition Matrix, the default is one
         self.P = np.ones([17, 20, 4])
-
+        
+    # initial the endstate, and describe by a [x, y]
     def endstate_init(self):
-        # 终点和撞墙为结束
         self.endstate = []
         self.endstate.append([0, 19])
         for i in range(9):
@@ -38,11 +39,13 @@ class game():
         for i in range(4):
             self.endstate.append([i, 14])
             self.endstate.append([16 - i, 7])
-
+    
+    # select an action and move to next state with a random policy
     def step_random(self):
         i = self.state[0]
         j = self.state[1]
         p = np.random.random()
+        # take an action and then save in self.a
         if i == 0:
             if j == 0:
                 if p < 0.5:
@@ -102,6 +105,7 @@ class game():
                 n = 2
             else:
                 n = 3
+        # change self.state according to self.a 
         if n == 0:
             self.a = 0
             self.state = [i, j + 1]
@@ -115,26 +119,24 @@ class game():
             self.a = 3
             self.state = [i - 1, j]
 
+    # select and move with greedy policy of State Value Function V
     def step_greedy_V(self):
         i = self.state[0]
         j = self.state[1]
         k = []
         if i == 0:
-            # 左上
             if j == 0:
                 m = max(self.V[i + 1][j], self.V[i][j + 1])
                 if m == self.V[j + 1][j]:
                     k.append(0)
                 if m == self.V[i + 1][j]:
                     k.append(1)
-            # 右上
             elif j == 19:
                 m = max(self.V[i + 1][j], self.V[i][j - 1])
                 if m == self.V[i + 1][j]:
                     k.append(1)
                 if m == self.V[i][j - 1]:
                     k.append(2)
-            # 上
             else:
                 m = max(self.V[i + 1][j], self.V[i][j + 1], self.V[i][j - 1])
                 if m == self.V[i][j + 1]:
@@ -144,21 +146,18 @@ class game():
                 if m == self.V[i][j - 1]:
                     k.append(2)
         elif i == 16:
-            # 左下
             if j == 0:
                 m = max(self.V[i][j + 1], self.V[i - 1][j])
                 if m == self.V[i][j + 1]:
                     k.append(0)
                 if m == self.V[i - 1][j]:
                     k.append(3)
-            # 右下
             elif j == 19:
                 m = max(self.V[i - 1][j], self.V[i][j - 1])
                 if m == self.V[i][j - 1]:
                     k.append(2)
                 if m == self.V[i - 1][j]:
                     k.append(3)
-            # 下
             else:
                 m = max(self.V[i - 1][j], self.V[i][j - 1], self.V[i][j + 1])
                 if m == self.V[i][j + 1]:
@@ -195,7 +194,6 @@ class game():
                 k.append(3)
         num = np.shape(k)[0]
         p = np.random.random()
-        # 选定 n 为最大值
         if num == 1:
             n = k[0]
         elif num == 2:
@@ -219,6 +217,7 @@ class game():
                 n = k[2]
             else:
                 n = k[3]
+        # save the index of the max as self.a, and the change the self.state
         if n == 0:
             self.a = 0
             self.state = [i, j + 1]
@@ -232,26 +231,24 @@ class game():
             self.a = 3
             self.state = [i - 1, j]
 
+    # select and move with greedy policy of Action-State Value Function Q        
     def step_greedy_Q(self):
         i = self.state[0]
         j = self.state[1]
         k = []
         if i == 0:
-            # 左上
             if j == 0:
                 m = max(self.Q[i][j][0], self.Q[i][j][1])
                 if m == self.Q[i][j][0]:
                     k.append(0)
                 if m == self.Q[i][j][1]:
                     k.append(1)
-            # 右上
             elif j == 19:
                 m = max(self.Q[i][j][2], self.Q[i][j][1])
                 if m == self.Q[i][j][1]:
                     k.append(1)
                 if m == self.Q[i][j][2]:
                     k.append(2)
-            # 上
             else:
                 m = max(self.Q[i][j][2], self.Q[i][j][1], self.Q[i][j][0])
                 if m == self.Q[i][j][0]:
@@ -261,21 +258,18 @@ class game():
                 if m == self.Q[i][j][2]:
                     k.append(2)
         elif i == 16:
-            # 左下
             if j == 0:
                 m = max(self.Q[i][j][0], self.Q[i][j][3])
                 if m == self.Q[i][j][0]:
                     k.append(0)
                 if m == self.Q[i][j][3]:
                     k.append(3)
-            # 右下
             elif j == 19:
                 m = max(self.Q[i][j][2], self.Q[i][j][3])
                 if m == self.Q[i][j][2]:
                     k.append(2)
                 if m == self.Q[i][j][3]:
                     k.append(3)
-            # 下
             else:
                 m = max(self.Q[i][j][2], self.Q[i][j][3], self.Q[i][j][0])
                 if m == self.Q[i][j][0]:
@@ -312,7 +306,6 @@ class game():
                 k.append(3)
         num = np.shape(k)[0]
         p = np.random.random()
-        # 选定 n 为最大值
         if num == 1:
             n = k[0]
         elif num == 2:
@@ -349,6 +342,7 @@ class game():
             self.a = 3
             self.state = [i - 1, j]
 
+    # select and move with epsilon-greedy policy of State Value Function V
     def step_epsilon_greedy_V(self, epsilon):
         p = np.random.random()
         if p > epsilon:
@@ -356,6 +350,7 @@ class game():
         else:
             self.step_random()
 
+    # select and move with epsilon-greedy policy of Actiion-State Value Function Q
     def step_epsilon_greedy_Q(self, epsilon):
         p = np.random.random()
         if p > epsilon:
@@ -363,6 +358,7 @@ class game():
         else:
             self.step_random()
 
+            
     def stra_evaluation(self, k):
         pass
 
